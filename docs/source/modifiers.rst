2b87627841fc1b1f4b2e77e6efb78edc5ca5dba5
Clement Julia <clement.julia13@gmail.com>
2020-02-18 22:21:12 +0000

    testing docs

--- docs/source/modifiers.rst
+++ docs/source/modifiers.rst
@@ -5,6 +5,4 @@ Modifiers
 A Modifier is the way the library refers to object that cause a change to an entity over a certain period of time. This distinction is important as while abilities can apply modifiers to change stats such as the player's current health they can also simply change it directly from the ability.
 
 .. autoclass:: pyzork.modifiers.Modifier
-    :members:
-    :inherited-members:
-    
+    :members: is_expired, buff, effect, add_buff, add_effect
--- pyzork/modifiers.py
+++ pyzork/modifiers.py
@@ -1,8 +1,33 @@
 from .enums import StatEnum
+from .entities import Entity
 
 class Modifier:
+    """Modifiers define changes to the stats of the entity they are attached to. They can do stuff like 
+    increase the attack of an ally for 3 turns or deal poison damage to an enemy over 5 turns. You can
+    stack as many different modifiers as you want on a single entity but you can never give an entity
+    the same modifier twice. This is the default behavior for every entity but it can be overwritten.
+    
+    Paramaters
+    -----------
+    duration : int
+        How many "turns" a buff lasts. A turn is all the players and enemies present in the battle
+        doing one action. This can be a positive int to demonstrate a temporary modifier which will
+        expire after a certain number of turns or it can be -1 to represent a permanent buff which
+        will only expire at the end of the battle.
+    stat_type : Optional[StatEnum]
+        An optional StatEnum that defines which stat this modifiers affect, this is only necessary
+        if you have defined the `buff` method.
+    name : Optional[str]
+        An optional string to give a name to the modifier, if this is not provided, the library will
+        fall back to the docstring of the class and if this isn't provided either it will fallback to
+        the nameof the class
+    description : Optional[str]
+        An optional string to give a short (or long) description about the buff and its effects. If
+        this is not provided the library will fall back to combining the docstring of the `buff`
+        and `effect` method    
+    """
     def __init__(self, **kwargs):
-        self.type = kwargs.get("type", StatEnum.null)
+        self.type = kwargs.get("stat_type", StatEnum.null)
         self.duration = kwargs.get("duration")
 
         if "name" in kwargs:
@@ -10,7 +35,7 @@ class Modifier:
         else:
             self.name = self.__doc__ if self.__doc__ else self.__class__.__name__
 
-        self.description = kwargs.get("description", self.__init__.__doc__)
+        self.description = kwargs.get("description", f"{self.buff.__doc__} {self.effect.__doc__}")
         
     def __hash__(self):
         return hash(self.name)
@@ -20,37 +45,64 @@ class Modifier:
             return NotImplemented
             
         return self.name == other.name
-        
+    
     def is_expired(self):
-        return duration == 0
+        """Simple method to check if is a buff is expired.
+        
+        Returns
+        --------
+        bool
+            Whether or not the modifier is expired
+        """
+        return self.duration == 0
 
-    def calc(self, player):
-        """This is normally the method that would have to be implemented by the user but because buffs are timed we use this 
-        to check if they're still valid before calling the logic."""
-        if self.duration != 0:
-            return self.buff(player)
+    def calc(self, entity : Entity):
+        """This is normally the method that would have to be implemented by the user but because 
+        buffs are timed we use this to check if they're still valid before calling the logic."""
+        if not self.is_expired():
+            return self.buff(entity)
 
-    def buff(self, player):
-        """Abstract method to be implemented on a per buff basis to determine the stat change."""
+    def buff(self, entity : Entity):
+        """Method to implement to provide a stat buff to an entity. This method is called everytime
+        the library calculates the stat that this modifier changes. There is no guarantee that this
+        method will only be called once per turn so don't make any permanent changes to the player 
+        such as restoring health or granting experience.
+        
+        Parameters
+        -----------
+        entity : Entity
+            The entity this modifier is attached to.
+        """
         pass
         
-    def effect(self, player):
-        """Abstract method to be implemented for turn based effects."""
+    def effect(self, entity):
+        """Method to implement to do a once per turn change to an entity. This method is called
+        exactly once per turn (given the buff is not expired) during the end turn phase. This is where
+        you can do stuff like restore or take away health from the entity or make other permanent changes
+        that the entity will carry on beyond this battle.
+        
+        Parameters
+        -----------
+        entity : Entity
+            The entity this modifier is attached to.
+        """
         pass
 
-    def end_turn(self, player):
+    def end_turn(self, entity):
         """Method called at the end of the turn for each buff to show time passed and left until the buff expires. 
         Permanent buffs are set to -1"""
         if self.duration > 0:
             self.duration -= 1
 
-        if self.duration == 0:
+        if self.is_expired():
             return
             
-        self.effect(player)
+        self.effect(entity)
         
     @classmethod
-    def add_effect(cls, duration, **kwargs):        
+    def add_effect(cls, duration, **kwargs):
+        """Decorator function to allow the user to define an effect by decorating a function. Takes the
+        same parameters as the class. """        
         def decorator(func):
             new_class = type(func.__name__, (cls,), {
                 "duration": duration, 
@@ -58,8 +110,6 @@ class Modifier:
                 "name": kwargs.get("name", func.__name__),
                 "description": kwargs.get("description", func.__doc__)
             })
-            # new_class.__init__ = __init__
-            # new_class.effect = func
             
             return new_class
             
@@ -67,19 +117,17 @@ class Modifier:
         
     @classmethod
     def add_buff(cls, duration, stat_type, **kwargs):
-        def __init__(self, **kwargs):
-            super().__init__(duration=duration, type=stat_type)
-        
+        """Decorator function to allow the user to define a buff by decorating a function. Takes
+        the same parameters as the class. Since this specifically adds a buff, the `stat_type` parameter
+        is required"""
         def decorator(func):
             new_class = type(func.__name__, (cls,), {
                 "duration": duration, 
-                "type": stat_type, 
+                "stat_type": stat_type, 
                 "buff": func,
                 "name": kwargs.get("name", func.__name__),
                 "description": kwargs.get("description", func.__doc__)
             })
-            # new_class.__init__ = __init__
-            # new_class.buff = func
             
             return new_class
             
